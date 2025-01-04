6b0ca37ba08e0acbe02738b1e4d4e02d06a7c9d7
Aridan Pantoja <aridanpantoja@gmail.com>
2025-01-04 01:21:28 -0300

    feat(layout): enhance root layout and implement QR code generator
    - Add responsive styling to html and body elements
    - Implement QR code generator with customizable options
    - Add form for URL input, color selection, and pet image choice
    - Display generated QR code with preview and download/share options

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { DM_Sans as DMSans } from 'next/font/google'
 import './globals.css'
+import { cn } from '@/lib/utils'
 
 export const metadata: Metadata = {
   title: 'Pet My Link | QR Code generator',
@@ -16,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={`${dmSans.className} antialiased`}>{children}</body>
+    <html lang="en" className="h-full">
+      <body
+        className={cn(`relative flex h-full antialiased`, dmSans.className)}
+      >
+        {children}
+      </body>
     </html>
   )
 }
