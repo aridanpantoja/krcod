5178c9bf0691993f0e9cf6cccfc8e017fee861f6
Aridan Pantoja <aridanpantoja@gmail.com>
2025-01-11 12:09:05 -0300

    feat(manifest): update icons for web app manifest
    - Remove favicon.ico icon
    - Add new PNG icons with sizes 92x92, 192x192, and 512x512
    - Update file paths for new icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,9 +12,19 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#452AAF',
     icons: [
       {
-        src: '/favicon.ico',
-        sizes: 'any',
-        type: 'image/x-icon',
+        src: '/favicon-96x96.png',
+        sizes: '92x92',
+        type: 'image/png',
+      },
+      {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
       },
     ],
   }
