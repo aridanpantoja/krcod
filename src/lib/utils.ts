8f2a9a7f9a0dec5ba8475759c1968e7254ce820d
Aridan Pantoja <aridanpantoja@gmail.com>
2025-01-12 23:57:57 -0300

    feat(analytics): add cookie consent banner and Google Analytics
    - Implement CookieBanner component for user consent
    - Add custom GoogleAnalytics component with consent management
    - Update layout to include CookieBanner and GoogleAnalytics
    - Add utility functions for local storage management
    - Implement Card component for UI
    - Update tailwind config with new screen size

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getLocalStorage(storageKey: string) {
+  try {
+    const storageValue = localStorage.getItem(storageKey)
+
+    if (!storageValue) throw new Error('No storage value found')
+
+    return JSON.parse(storageValue)
+  } catch {
+    return null
+  }
+}
+
+export function setLocalStorage(storageKey: string, value: string) {
+  try {
+    localStorage.setItem(storageKey, value)
+  } catch {
+    return null
+  }
+}
